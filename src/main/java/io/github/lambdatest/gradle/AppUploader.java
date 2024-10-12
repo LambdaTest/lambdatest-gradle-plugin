73a28c3fcfc72fa8cac59bf9fc1c35fefca2bd36
Tanish Azad <tanishazad03@gmail.com>
2024-10-12 08:19:00 +0530

    fix deprecation warnings and cleanup code

--- src/main/java/io/github/lambdatest/gradle/AppUploader.java
+++ src/main/java/io/github/lambdatest/gradle/AppUploader.java
@@ -1,10 +1,7 @@
 package io.github.lambdatest.gradle;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
-import okhttp3.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -30,39 +27,10 @@ public class AppUploader {
         return CompletableFuture.supplyAsync(
                 () -> {
                     try {
-                        OkHttpClient client = new OkHttpClient().newBuilder().build();
-                        MediaType mediaType = MediaType.parse("application/octet-stream");
-                        RequestBody body =
-                                new MultipartBody.Builder()
-                                        .setType(MultipartBody.FORM)
-                                        .addFormDataPart(
-                                                "appFile",
-                                                appFilePath,
-                                                RequestBody.create(
-                                                        mediaType, new java.io.File(appFilePath)))
-                                        .addFormDataPart("type", "espresso-android")
-                                        .build();
-                        Request request =
-                                new Request.Builder()
-                                        .url(Constants.API_URL)
-                                        .method("POST", body)
-                                        .addHeader(
-                                                "Authorization",
-                                                Credentials.basic(username, accessKey))
-                                        .build();
-                        try (Response response = client.newCall(request).execute()) {
-                            if (!response.isSuccessful())
-                                throw new IOException("Unexpected code " + response);
-
-                            // Parse the JSON response and extract the app_id
-                            String responseBody = response.body().string();
-                            JsonObject jsonObject =
-                                    JsonParser.parseString(responseBody).getAsJsonObject();
-                            String appId = jsonObject.get("app_id").getAsString();
-
-                            logger.info("Uploaded app ID: {}", appId);
-                            return appId;
-                        }
+                        String appId =
+                                UploaderUtil.uploadAndGetId(username, accessKey, appFilePath);
+                        logger.info("Uploaded app ID: {}", appId);
+                        return appId;
                     } catch (IOException e) {
                         logger.error("Error uploading app: {}", e.getMessage());
                         throw new RuntimeException(e);
--- src/main/java/io/github/lambdatest/gradle/Constants.java
+++ src/main/java/io/github/lambdatest/gradle/Constants.java
@@ -1,6 +1,11 @@
 package io.github.lambdatest.gradle;
 
 public class Constants {
+    private Constants() {
+        throw new UnsupportedOperationException(
+                "This is a utility class and cannot be instantiated");
+    }
+
     public static final String API_URL = "https://manual-api.lambdatest.com/app/uploadFramework";
     public static final String BUILD_URL =
             "https://mobile-api.lambdatest.com/framework/v1/espresso/build";
